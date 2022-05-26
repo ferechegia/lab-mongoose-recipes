36de4142e535215778856f3c63bbbfc3634a2fa2
Fernando Echevarria Gianello <fernandoechevarriagianello@iMac-de-Fernando.local>
2022-05-26 17:57:53 +0200

    done

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+const options = {
+}
+
+let newRec = {
+  title: 'Fernandos food',
+  level: 'Easy Peasy',
+  ingredients: ['Fernando'],
+  cuisine: 'Asian',
+  dishType:'breakfast',
+  image: "https://images.media-allrecipes.com/images/75131.jpg",
+  creator: 'Fernando',
+}
+
 // Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI)
@@ -17,7 +30,28 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create(newRec).then((responseDB) => {
+      console.log('Fernando recipe', responseDB)
+      return Recipe.insertMany(data);
+    }).then(() => {
+      return Recipe.findOneAndUpdate(
+        { title: "Rigatoni alla Genovese" },
+        { duration: 100}
+      )
+    })
+    .then((recipe) => {
+      console.log(`The ${recipe.title} has been updated`);
+    })
+    .then((deleted) => {
+      return Recipe.deleteOne({ 
+        title: "Carrot Cake"
+      })
+    })
+    .then((deleted) => {
+      console.log(`Oh no! ${deleted.title} is no longer available`);
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+  .finally(() => mongoose.connection.close());
+  })
